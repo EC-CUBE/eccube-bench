3a7fe41e35544e270780ab7417495e19d977c385
kiy0taka <kiy0taka333@gmail.com>
2018-11-05 13:09:17 +0900

    CentOS 7.5にアップグレード

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const randomstring = require('randomstring');
 const SlackClient = require('@slack/client').WebClient;
 
-const ARCHIVE_ID_CENTOS_7_4_64 = '113000629235'
+const ARCHIVE_ID_CENTOS_7_5_64 = '113001307003'
 const SSD_PLAN_ID = 4;
 const ZONE_ID = 21001; // 東京第1
 const SERVER_PLAN_ID_1CORE_1G = 1001;
@@ -122,7 +122,7 @@ function createDisk(serverId, serverName) {
                 Connection: 'virtio',
                 SizeMB: 20480,
                 SourceArchive: {
-                    ID: ARCHIVE_ID_CENTOS_7_4_64
+                    ID: ARCHIVE_ID_CENTOS_7_5_64
                 },
                 Plan: { ID: SSD_PLAN_ID }
             }
@@ -366,7 +366,7 @@ co(function* () {
         'path': 'archive'
     })
 
-    let found = archives.response.archives.filter(data => { return data.id == ARCHIVE_ID_CENTOS_7_4_64; });
+    let found = archives.response.archives.filter(data => { return data.id == ARCHIVE_ID_CENTOS_7_5_64; });
     if (!found.length) {
         throw new Error(`Archive ID not found.`);
     }
